1580892ba3af36c0247b4175f7012ad0b2cc758f
Hasankhan170 <hasankhanali0316@gmail.com>
2024-10-06 15:54:15 +0500

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 
 
 
@@ -10,14 +12,36 @@ const page = async ()=>{
   console.log(response);
   
   
+  
  
   
 
   return (
     <>
-    <h1>Meme Maker</h1>
+      <h1 className="text-center text-3xl font-bold m-5">Meme Maker</h1>
+      <div className="p-3 flex flex-wrap justify-center gap-5">
+        {
+          response ? response.memes.map((item: { name: string; id: string; url: string }) => {
+            return (
+              <div key={item.id} className="flex flex-col items-center">
+                <Image 
+                  className="w-48 h-48 object-cover border" 
+                  src={item.url} 
+                  alt={item.name} 
+                  width={200} 
+                  height={200} 
+                />
+                <button 
+                  className="mt-2 w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"><Link href={}>Meme Generate</Link></button>
+              </div>
+            );
+          }) : <p>No data Found...</p>
+        }
+      </div>
     </>
-  )
+  );
+  
+  
 }
 
 export default page;
